41a4792012bb985d127f0209edba6896bbfae158
chenxuesdu <chenxuesdu@gmail.com>
2015-11-07 21:31:28 -0800

    specialball

---
+++ SpecialChaser.java
@@ -0,0 +1,40 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class SpecialChaser here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SpecialChaser extends Chaser
+{
+    private int dr;
+    private int hp;
+    private GreenfootImage image;
+    public GreenfootImage getImage() {
+        return image;
+    }
+    public SpecialChaser() {
+        image = new GreenfootImage(30,30);
+    }
+   
+    public int getDr() {
+        return dr;
+    }
+    public void setDr(int dr) {
+        this.dr = dr;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public void draw() {
+        image.setColor(new java.awt.Color(0, 0, 0));
+        image.fillOval(0, 0, 30, 30);
+        image.setColor(new java.awt.Color(224, 240, 255));
+        image.fillOval(7, 7, 16, 16);
+        setImage(image);
+    } 
+}
